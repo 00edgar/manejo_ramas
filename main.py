e61fb5c768580583837b83ad0d642bd9b71baaaa
00edgar <shyzedgar@gmail.com>
2026-03-09 22:19:31 -0600

    feat: :sparkles: actualizar
    permite actualizar los datos de  los productos

--- main.py
+++ main.py
@@ -9,4 +9,14 @@ def mostrar_producto(productos):
         for producto in productos:
             print(f"Nombre: {producto['nombre']}, Precio: {producto['precio']}, Cantidad: {producto['cantidad']}")  
 
-
+def actualizar_producto(productos):
+    nombre = input("Ingrese el nombre del producto a actualizar: ")
+    for producto in productos:
+        if producto["nombre"] == nombre:
+            nuevo_precio = float(input("Ingrese el nuevo precio del producto: "))
+            nueva_cantidad = int(input("Ingrese la nueva cantidad del producto: "))
+            producto["precio"] = nuevo_precio
+            producto["cantidad"] = nueva_cantidad
+            print("Producto actualizado exitosamente.")
+            return
+    print("Producto no encontrado.")
